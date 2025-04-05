bc9ba53f8ad0bca2ebaf6761165f5135743c3ad8
steeeeeve01 <136960831+steeeeeve01@users.noreply.github.com>
2025-04-05 23:05:20 +0800

    course 05/06 cards matched AspectVGrid griditemsizethatfit GeometryReader

---
+++ memirize_stanfordcourse_swift/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  memirize_stanfordcourse_swift
+//
+//  Created by 簡紹益 on 2025/4/2.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
